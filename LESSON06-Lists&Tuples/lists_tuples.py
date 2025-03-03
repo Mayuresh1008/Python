b08edc10062bcc1fd4e6eef833a5fdec0a10bad5
Mayuresh Patil <mayuresh.patil1008@gmail.com>
2025-03-03 23:36:09 +0530

    added the folder

---
+++ LESSON06-Lists&Tuples/lists_tuples.py
@@ -0,0 +1,126 @@
+# Lists
+users = ['Dave', 'John', 'Sara']
+
+data = ['Dave', 42, True]
+
+emptylist = []
+
+print("Dave" in emptylist)
+
+print(users[0])
+print(users[-2])
+
+print(users.index('Sara'))
+
+print(users[0:2])
+print(users[1:])
+print(users[-3:-1])
+
+print(len(data))
+
+users.append('Elsa')
+print(users)
+
+users += ['Jason']
+print(users)
+
+users.extend(['Robert','Jimmy'])
+print(users)
+
+# users.extend(data)
+# print(users)
+
+users.insert(0, 'Bob')
+print(users)
+
+users[2:2] = ['Eddie', 'Alex']
+print(users)
+
+users[1:3] = ['Robert', 'JPJ']
+print(users)
+
+users.remove('Bob')
+print(users)
+
+print(users.pop())
+print(users)
+
+del users[0]
+print(users)
+
+# del data
+data.clear() # clearing the list and making the list empty
+print(data)
+
+users[1:2] = ['dave']
+print(users)
+
+users.sort() # sorting the list in ascending order in normal manner
+print(users)
+
+users.sort(key=str.lower) # sorting the list in ascending order in case insensitive manner
+print(users)
+
+nums = [4, 42, 78, 1, 5]
+nums.reverse()
+print(nums)
+
+# nums.sort(reverse=True)
+# print(nums)
+
+# sorting or reversing the list without disturbing the original one 
+print(sorted(nums, reverse=True))
+print(nums)
+
+# Another approach by using copy
+# Three different ways of copy
+numscopy = nums.copy()
+mynums = list(nums)
+mycopy = nums[:]
+
+print(numscopy)
+print(mynums)
+mycopy.sort()
+print(mycopy)
+print(nums)
+
+print(type(nums))
+
+# Creating list using constructor
+mylist = list([1, "Neil", True])
+print(mylist)  
+
+# Tuples
+mytuple = tuple(("Dave", 42, True))
+
+anothertuple = (1, 4, 2, 8, 2, 2)
+
+print(mytuple)
+print(type(mytuple))
+print(type(anothertuple))
+
+# Tuples can't be changed like lists but we can do it using list and copy
+newlist = list(mytuple)
+newlist.append('Neil')
+newtuple = tuple(newlist)
+print(newtuple)
+
+# Unpacking the tuple
+(one, *two, hey) = anothertuple
+print(one)
+print(two)
+print(hey)
+
+print(anothertuple.count(2))
+print(anothertuple.index(2))
+
+
+
+
+
+
+
+
+
+
+
